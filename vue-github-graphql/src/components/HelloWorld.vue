9544daa2c288548a01e9574a128c01ea6cc40df8
mehmeteyupoglu2 <mehmeteyupoglu2@gmail.com>
2020-08-18 01:41:04 +0300

    Update UI, add query variables

--- vue-github-graphql/src/components/HelloWorld.vue
+++ vue-github-graphql/src/components/HelloWorld.vue
@@ -2,11 +2,21 @@
   <v-container>
     <v-app>
       <v-layout row wrap>
+        <v-overlay v-if="$apollo.loading">
+          <div v-if="$apollo.loading">
+            <v-progress-circular
+              :size="70"
+              :width="7"
+              indeterminate
+            ></v-progress-circular>
+          </div>
+        </v-overlay>
+
         <v-card
           color="grey lighten-4"
           class="d-flex flex-row flex-wrap mb-5 mr-3 ml-2  "
-          max-width="330"
-          max-height="200"
+          max-width="335"
+          max-height="325"
           v-for="(item, i) in search.edges"
           :key="i"
           tile
@@ -25,6 +35,9 @@
                   : "No description in this repository"
               }}</v-list-item-subtitle>
             </v-list-item-content>
+            <v-list-item-avatar size="40">
+              <v-img :src="item.node.owner.avatarUrl"></v-img>
+            </v-list-item-avatar>
           </v-list-item>
 
           <v-card-actions>
@@ -52,31 +65,30 @@
         </v-card>
       </v-layout>
     </v-app>
+    <!-- {{ search.edges }} -->
   </v-container>
 </template>
 
 <script>
-import { Sample } from "../graphql/sample";
-import { Search } from "../graphql/Search";
+import { SEARCH } from "../graphql/Search";
 export default {
   name: "HelloWorld",
-  apollo: {
-    viewer: Sample,
-    search: Search,
-  },
-
+  props: ["searchString"],
   data() {
     return {
-      viewer: [],
       search: [],
+      queryString: "",
     };
   },
-  // computed: {
-  //   filteredRepository() {
-  //     return this.viewer.repositories.edges.filter((item) =>
-  //       item.node.name.match(this.search)
-  //     );
-  //   },
-  // },
+  apollo: {
+    search: {
+      query: SEARCH,
+      variables() {
+        return {
+          queryString: "language:" + this.searchString + " stars:>10000",
+        };
+      },
+    },
+  },
 };
 </script>
